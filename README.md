27700429fdf78deddca8b2f932d27908bcbc5898
Shankar Rao Mata <mshankarrao1970@gmail.com>
2021-09-14 16:57:25 +0000

    adding to github repo

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Academy
-Practicals
+
--- TruffleIntroduction/contracts/VolcanoCoin.sol
+++ TruffleIntroduction/contracts/VolcanoCoin.sol
@@ -1,46 +1,91 @@
-// SPDX-License-Identifier: UNLICENSED
-
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract VolcanoCoin is ERC20("Volcano", "VLC"), Ownable {
-    uint256 tokenSupply = 100;
-    event tokenSupplyChange(uint256);
-    mapping(address => uint256) public balances;
-    mapping(address => Payment[]) public paymentsRecord;
-
-    struct Payment {
-        uint256 amount;
-        address recipient;
+contract VolcanoCoin is ERC20("Volcano","VCN"), Ownable{
+    
+    event TotalSupply(uint _totalSupply);
+    
+    event Transfer(uint _amount, address _recipientAddress);
+    
+    enum PaymentType {UNKNOWN,BASICPAYMENT,REFUND,DIVIDEND,GROUPPAYMENT}
+    
+    mapping(address => uint) balances;
+    
+    address administrator;
+    
+    struct Payment{
+        uint paymentId;
+        PaymentType paymentType;
+        string comment;
+        uint amount;
+        address recipentAddress;
+        uint256 timestamp;
     }
+    
+    mapping(address => Payment[]) payments;
 
-    constructor() {
-        _mint(msg.sender, tokenSupply);
+    
+    constructor(){
+        administrator = msg.sender;
+       _mint(owner(),10000);
+       balances[owner()] = totalSupply();
     }
-
-    function getTokenSupply() public view returns (uint256) {
-        return tokenSupply;
+    
+    //6 answer ,the ivestor would think about the inflation issues that there is no limit in number of tokens 
+    
+    function increaseSupply(uint256 tokens) public onlyOwner {
+        _mint(owner(),tokens);
+        emit TotalSupply(totalSupply());
     }
-
-    function changeTokenSupply() public onlyOwner {
-        tokenSupply = 300;
-        emit tokenSupplyChange(tokenSupply);
+    
+    function transfer(address _recipientAddress, uint _amount) override public returns (bool) {
+        require(balanceOf(msg.sender) > _amount, "Transfer amount exceeds balance");
+        _transfer(msg.sender,_recipientAddress,_amount);
+        Payment memory newPayment;
+        newPayment.paymentId = payments[msg.sender].length;
+        newPayment.amount = _amount;
+        newPayment.recipentAddress = _recipientAddress; 
+        newPayment.paymentType = PaymentType.UNKNOWN;
+        newPayment.comment = '';
+        newPayment.timestamp = block.timestamp;
+        payments[msg.sender].push(newPayment);
+        emit Transfer(_amount,msg.sender);
+        return true;
     }
-
-    function mintTokens() public onlyOwner {
-        _mint(msg.sender, tokenSupply);
+    
+    function updatePayment(uint _paymentId, PaymentType _paymentType, string memory _comment) public returns (bool) {
+        require(_paymentId > 0,"PaymentId must be > 0");
+        
+        Payment storage payment = payments[msg.sender][_paymentId];
+        payment.paymentType = _paymentType;
+         payment.comment = _comment;
+        
+      return true;
     }
-
-    function makeTransfer(address _recipient, uint256 _amount) public {
-        _transfer(msg.sender, _recipient, _amount);
-        balances[msg.sender] -= _amount;
-        addPaymentRecord(_recipient, _amount);
+    
+    
+    function updateByAdmin(uint _paymentId, PaymentType _paymentType, address updateAddress) public returns (bool){
+        require(msg.sender == administrator, "Only administrator is allowed");
+        
+        Payment storage payment = payments[updateAddress][_paymentId];
+        payment.paymentType = _paymentType;
+        payment.comment = string(
+            abi.encodePacked(
+                payment.comment,
+                " updated by ",
+                abi.encodePacked(msg.sender)
+            )
+        );
+        
+        return true;
+        
     }
-
-    function addPaymentRecord(address _recipient, uint256 _amount) internal {
-        Payment memory newPayment = Payment(_amount, _recipient);
-        paymentsRecord[msg.sender].push(newPayment);
+    
+    
+   function getPayment() public view returns (Payment[] memory) {
+        return payments[msg.sender];
     }
 }
